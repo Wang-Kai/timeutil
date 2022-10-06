475951c7f547f92f124404cd538c5c4c2df5522e
王凯 <wangkai05@bilibili.com>
2022-10-06 21:39:21 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Wang-Kai/timeutil
+
+go 1.18
---
+++ gotime.go
@@ -0,0 +1,46 @@
+package gotime
+
+import "time"
+
+type TimeUnit uint8
+
+const (
+	Y TimeUnit = iota
+	M
+	D
+	H
+)
+
+func StartOf(t time.Time, unit TimeUnit) time.Time {
+	var nt time.Time
+	switch unit {
+	case Y:
+		y := t.Year()
+		nt = time.Date(y, 1, 1, 0, 0, 0, 0, t.Location())
+	case M:
+		y, m := t.Year(), t.Month()
+		nt = time.Date(y, m, 1, 0, 0, 0, 0, t.Location())
+	case D:
+		y, m, d := t.Year(), t.Month(), t.Day()
+		nt = time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+	case H:
+		y, m, d, h := t.Year(), t.Month(), t.Day(), t.Hour()
+		nt = time.Date(y, m, d, h, 0, 0, 0, t.Location())
+	}
+	return nt
+}
+
+func EndOf(t time.Time, unit TimeUnit) time.Time {
+	var nt time.Time
+	switch unit {
+	case Y:
+		nt = StartOf(t, Y).AddDate(1, 0, 0).Add(time.Nanosecond * -1)
+	case M:
+		nt = StartOf(t, M).AddDate(0, 1, 0).Add(time.Nanosecond * -1)
+	case D:
+		nt = StartOf(t, D).AddDate(0, 0, 1).Add(time.Nanosecond * -1)
+	case H:
+		nt = StartOf(t, H).Add(time.Hour - time.Nanosecond)
+	}
+	return nt
+}
